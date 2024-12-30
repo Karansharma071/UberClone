f2089da886e3dcf3be1d5aeff0b7380b9e1145c7
Karansharma071 <karan071sharma@gmail.com>
2024-12-30 21:53:35 +0530

    user authentication backend

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.routes')
+const cookieParser = require('cookie-parser')
 connectToDb()
 app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('Helloo World')
---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/user.model')
+const blacklistModel = require('../models/blacklistToken.model') // Import the blacklist model
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+module.exports.authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.header('Authorization')?.split(' ')[1]
+  if (!token) {
+    return res.status(401).send({ error: 'Unauthorized' })
+  }
+  try {
+    // Check if the token is in the blacklist
+    const isblacklistedToken = await blacklistModel.findOne({ token })
+    if (isblacklistedToken) {
+      return res.status(401).send({ error: 'Unauthorized' })
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const user = await userModel.findById(decoded._id)
+    if (!user) {
+      throw new Error()
+    }
+    req.user = user
+    next()
+  } catch (error) {
+    res.status(401).send({ error: 'Unauthorized' })
+  }
+}
